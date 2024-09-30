6c5b4385f3bf8a5e97615a7f058ec79683394350
shivam8112005 <shivamshukla8112005@gmail.com>
2024-09-30 17:43:46 +0530

    created currencyInfo hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const [amount, setAmount]=useState(0);
+  const [from, setFrom]=useState("usd");
+  const [to, setTo]=useState("inr");
+  const [convertedAMount, setConvertedAmount]=useState(0);
+
+  const currencyInfo=useCurrencyInfo(from);
+
+  const options=Object.keys(currencyInfo);
 
   return (
     <>
